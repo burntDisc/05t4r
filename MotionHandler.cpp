8ed621bc7d5e542976052b70c0e051b8a31d0404
gpucigarette <ellistonfranks@gmail.com>
2023-01-11 14:39:10 -0800

    Removinf sphere check (doesn't handle edge cases)

--- MotionHandler.cpp
+++ MotionHandler.cpp
@@ -15,19 +15,15 @@ glm::vec3 MotionHandler::ApplyTranslation(glm::vec3 start, glm::vec3 destination
 	for (int meshIndex = 0; meshIndex < collisionMeshes.size(); ++meshIndex)
 	{
 		CollisionMesh& mesh = collisionMeshes[meshIndex];
-		float meshDistance = glm::length(mesh.centerPoint - start);
-		if (meshDistance < traversalDistance + mesh.radius)
+		glm::vec3 adjustedPosition = mesh.GetAdjustedDestination(start, destination, normal);
+		if (adjustedPosition != destination)
 		{
-			glm::vec3 adjustedPosition = mesh.GetAdjustedDestination(start, destination, normal);
-			if (adjustedPosition != destination)
-			{
-				adjustedPositionsSum += adjustedPosition;
-				++numAdjustedPositions;
-			}
-			else
-			{
-				normal = glm::vec3(0.0, 0.0, 0.0);
-			}
+			adjustedPositionsSum += adjustedPosition;
+			++numAdjustedPositions;
+		}
+		else
+		{
+			normal = glm::vec3(0.0, 0.0, 0.0);
 		}
 	}
 	if (numAdjustedPositions > 0)
