42571a2d96bd692ebdae094fc636e302a8cf87ac
gpucigarette <ellistonfranks@gmail.com>
2023-01-30 02:12:24 -0800

    NIT: gravity

--- Camera.h
+++ Camera.h
@@ -65,7 +65,7 @@ private:
 	const float maxSpeed = 5.0f;
 	const float collisionFriction = 2.0f;
 	const float baseFriction = 0.1f;
-	const float gravity = 0.09f;
+	const float gravity = 0.12f;
 	const float repulsionFac = 1.5f;
 	const glm::vec3 modelAdjustment = glm::vec3(0.0f, 5.0f, 0.0f);
 
