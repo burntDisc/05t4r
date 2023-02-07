19e8f0de5f824b3319b4962138c2a4409dbdaee0
gpucigarette <ellistonfranks@gmail.com>
2023-02-06 18:53:04 -0800

    Fixing model path

--- Main.cpp
+++ Main.cpp
@@ -139,7 +139,7 @@ int main()
 	);
 
 	// Create Opponent object
-	std::string oppModelPath = parentDir + "/models/statue/prototype/scene.gltf";
+	std::string oppModelPath = parentDir + "/models/statue/scene.gltf";
 	glm::vec3 oppTranslation(0.0f, 50.0f, 50.0f);
 	glm::quat oppRotation = glm::vec3(0.0f, -acos(0), 0.0f);
 	glm::vec3 oppScale(10.0f, 10.0f, 10.0f);
