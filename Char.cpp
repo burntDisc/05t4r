4ae5477f115f03ba361eb67106dcf627198fccb8
gpucigarette <ellistonfranks@gmail.com>
2023-03-11 02:17:41 -0800

    filling out number graphics

--- Char.cpp
+++ Char.cpp
@@ -1,13 +1,27 @@
 #include "Char.h"
 
 //TODO: Batch Draw Chars
+
+//TODO: fix weird scaling from center (height of 2)
 Char::Char(char character, glm::vec2 origin, float size)
 {
 	//build character
     switch (character) 
 	{
+		case '.':
+			Init_dot();
+			break;
 		case '0':
 			Init_0();
+			break;
+		case '1':
+			Init_1();
+			break;
+		case '2':
+			Init_2();
+			break;
+		case '3':
+			Init_3();
 			break;
 		case '4':
 			Init_4();
@@ -15,11 +29,17 @@ Char::Char(char character, glm::vec2 origin, float size)
 		case '5':
 			Init_5();
 			break;
-		case 'a':
-			Init_a();
+		case '6':
+			Init_6();
 			break;
-		case 'b':
-			// code block
+		case '7':
+			Init_7();
+			break;
+		case '8':
+			Init_8();
+			break;
+		case '9':
+			Init_9();
 			break;
 		case 'r':
 			Init_r();
@@ -41,6 +61,21 @@ Char::Char(char character, glm::vec2 origin, float size)
 	SetVertices();
 }
 
+void Char::Init_dot()
+{
+	indices.push_back(0);
+	indices.push_back(1);
+	indices.push_back(2);
+
+	indices.push_back(2);
+	indices.push_back(3);
+	indices.push_back(0);
+
+	vertices.push_back(Vertex2D(glm::vec2(0.25f, 0.0f), glm::vec3(0.0f, 0.0f, 0.0f)));
+	vertices.push_back(Vertex2D(glm::vec2(0.25f, 0.25f), glm::vec3(0.0f, 0.0f, 0.0f)));
+	vertices.push_back(Vertex2D(glm::vec2(0.5f, 0.25f), glm::vec3(0.0f, 0.0f, 0.0f)));
+	vertices.push_back(Vertex2D(glm::vec2(0.5f, 0.0f), glm::vec3(0.0f, 0.0f, 0.0f)));
+}
 
 void Char::Init_0()
 {
@@ -60,12 +95,102 @@ void Char::Init_0()
 	indices.push_back(3);
 	indices.push_back(4);
 
-	vertices.push_back(Vertex2D(glm::vec2(0.50f, 0.75f), glm::vec3(0.0f, 0.0f, 0.0f)));
-	vertices.push_back(Vertex2D(glm::vec2(0.75f, 0.0f), glm::vec3(0.0f, 0.0f, 0.0f)));
-	vertices.push_back(Vertex2D(glm::vec2(1.0f, 0.0f), glm::vec3(0.0f, 0.0f, 0.0f)));
+	vertices.push_back(Vertex2D(glm::vec2(0.50f, 1.0f), glm::vec3(0.0f, 0.0f, 0.0f)));
+	vertices.push_back(Vertex2D(glm::vec2(0.75f, 0.50f), glm::vec3(0.0f, 0.0f, 0.0f)));
+	vertices.push_back(Vertex2D(glm::vec2(1.0f, 0.50f), glm::vec3(0.0f, 0.0f, 0.0f)));
+	vertices.push_back(Vertex2D(glm::vec2(0.0f, 0.50f), glm::vec3(0.0f, 0.0f, 0.0f)));
+	vertices.push_back(Vertex2D(glm::vec2(0.25f, 0.50f), glm::vec3(0.0f, 0.0f, 0.0f)));
+	vertices.push_back(Vertex2D(glm::vec2(0.50f, 0.0f), glm::vec3(0.0f, 0.0f, 0.0f)));
+}
+
+void Char::Init_1()
+{
+	indices.push_back(0);
+	indices.push_back(1);
+	indices.push_back(2);
+
+	indices.push_back(2);
+	indices.push_back(3);
+	indices.push_back(4);
+
+	indices.push_back(5);
+	indices.push_back(6);
+	indices.push_back(7);
+
+	vertices.push_back(Vertex2D(glm::vec2(0.2f, 0.6f), glm::vec3(0.0f, 0.0f, 0.0f)));
+	vertices.push_back(Vertex2D(glm::vec2(0.6f, 1.0f), glm::vec3(0.0f, 0.0f, 0.0f)));
+	vertices.push_back(Vertex2D(glm::vec2(0.6f, 0.6f), glm::vec3(0.0f, 0.0f, 0.0f)));
+
+	vertices.push_back(Vertex2D(glm::vec2(0.4f, 0.6f), glm::vec3(0.0f, 0.0f, 0.0f)));
+	vertices.push_back(Vertex2D(glm::vec2(0.5f, 0.0f), glm::vec3(0.0f, 0.0f, 0.0f)));
+
+	vertices.push_back(Vertex2D(glm::vec2(0.2f, 0.0f), glm::vec3(0.0f, 0.0f, 0.0f)));
+	vertices.push_back(Vertex2D(glm::vec2(0.5f, 0.2f), glm::vec3(0.0f, 0.0f, 0.0f)));
+	vertices.push_back(Vertex2D(glm::vec2(0.8f, 0.0f), glm::vec3(0.0f, 0.0f, 0.0f)));
+}
+
+void Char::Init_2()
+{
+	//top
+	indices.push_back(0);
+	indices.push_back(1);
+	indices.push_back(2);
+
+	// down angled part
+	indices.push_back(2);
+	indices.push_back(3);
+	indices.push_back(4);
+
+	indices.push_back(2);
+	indices.push_back(4);
+	indices.push_back(5);
+
+	// bottom
+	indices.push_back(4);
+	indices.push_back(5);
+	indices.push_back(6);
+
+	indices.push_back(5);
+	indices.push_back(6);
+	indices.push_back(7);
+
+	vertices.push_back(Vertex2D(glm::vec2(0.0f, 0.7f), glm::vec3(0.0f, 0.0f, 0.0f)));
+	vertices.push_back(Vertex2D(glm::vec2(0.5f, 1.0f), glm::vec3(0.0f, 0.0f, 0.0f)));
+	vertices.push_back(Vertex2D(glm::vec2(1.0f, 0.7f), glm::vec3(0.0f, 0.0f, 0.0f)));
+
+	vertices.push_back(Vertex2D(glm::vec2(0.8f, 0.7f), glm::vec3(0.0f, 0.0f, 0.0f)));
+	vertices.push_back(Vertex2D(glm::vec2(0.0f, 0.1f), glm::vec3(0.0f, 0.0f, 0.0f)));
+
 	vertices.push_back(Vertex2D(glm::vec2(0.0f, 0.0f), glm::vec3(0.0f, 0.0f, 0.0f)));
-	vertices.push_back(Vertex2D(glm::vec2(0.25f, 0.0f), glm::vec3(0.0f, 0.0f, 0.0f)));
-	vertices.push_back(Vertex2D(glm::vec2(0.50f, -0.75f), glm::vec3(0.0f, 0.0f, 0.0f)));
+
+	vertices.push_back(Vertex2D(glm::vec2(1.0f, 0.1f), glm::vec3(0.0f, 0.0f, 0.0f)));
+	vertices.push_back(Vertex2D(glm::vec2(1.0f, 0.0f), glm::vec3(0.0f, 0.0f, 0.0f)));
+}
+
+void Char::Init_3()
+{
+	indices.push_back(0);
+	indices.push_back(1);
+	indices.push_back(2);
+
+	indices.push_back(3);
+	indices.push_back(4);
+	indices.push_back(5);
+
+	indices.push_back(6);
+	indices.push_back(7);
+	indices.push_back(2);
+
+	vertices.push_back(Vertex2D(glm::vec2(0.0f, 1.0f), glm::vec3(0.0f, 0.0f, 0.0f)));
+	vertices.push_back(Vertex2D(glm::vec2(0.9f, 1.0f), glm::vec3(0.0f, 0.0f, 0.0f)));
+	vertices.push_back(Vertex2D(glm::vec2(0.9f, 0.5f), glm::vec3(0.0f, 0.0f, 0.0f)));
+
+	vertices.push_back(Vertex2D(glm::vec2(0.9f, 0.75f), glm::vec3(0.0f, 0.0f, 0.0f)));
+	vertices.push_back(Vertex2D(glm::vec2(0.0f, 0.5f), glm::vec3(0.0f, 0.0f, 0.0f)));
+	vertices.push_back(Vertex2D(glm::vec2(0.9f, 0.25f), glm::vec3(0.0f, 0.0f, 0.0f)));
+
+	vertices.push_back(Vertex2D(glm::vec2(0.0f, 0.0f), glm::vec3(0.0f, 0.0f, 0.0f)));
+	vertices.push_back(Vertex2D(glm::vec2(0.9f, 0.0f), glm::vec3(0.0f, 0.0f, 0.0f)));
 }
 
 void Char::Init_4()
@@ -82,14 +207,14 @@ void Char::Init_4()
 	indices.push_back(4);
 	indices.push_back(5);
 
-	vertices.push_back(Vertex2D(glm::vec2(0.75f, 0.75f), glm::vec3(0.0f, 0.0f, 0.0f)));
-	vertices.push_back(Vertex2D(glm::vec2(0.5f, 0.25f), glm::vec3(0.0f, 0.0f, 0.0f)));
-	vertices.push_back(Vertex2D(glm::vec2(0.0f, 0.0f), glm::vec3(0.0f, 0.0f, 0.0f)));
+	vertices.push_back(Vertex2D(glm::vec2(0.75f, 1.0f), glm::vec3(0.0f, 0.0f, 0.0f)));
+	vertices.push_back(Vertex2D(glm::vec2(0.5f, 0.66), glm::vec3(0.0f, 0.0f, 0.0f)));
+	vertices.push_back(Vertex2D(glm::vec2(0.0f, 0.5f), glm::vec3(0.0f, 0.0f, 0.0f)));
 
-	vertices.push_back(Vertex2D(glm::vec2(1.0f, 0.0f), glm::vec3(0.0f, 0.0f, 0.0f)));
+	vertices.push_back(Vertex2D(glm::vec2(1.0f, 0.5f), glm::vec3(0.0f, 0.0f, 0.0f)));
 
-	vertices.push_back(Vertex2D(glm::vec2(0.5f, 0.0f), glm::vec3(0.0f, 0.0f, 0.0f)));
-	vertices.push_back(Vertex2D(glm::vec2(0.75f, -0.75f), glm::vec3(0.0f, 0.0f, 0.0f)));
+	vertices.push_back(Vertex2D(glm::vec2(0.5f, 0.5f), glm::vec3(0.0f, 0.0f, 0.0f)));
+	vertices.push_back(Vertex2D(glm::vec2(0.75f, 0.0f), glm::vec3(0.0f, 0.0f, 0.0f)));
 }
 
 void Char::Init_5()
@@ -110,33 +235,134 @@ void Char::Init_5()
 	indices.push_back(3);
 	indices.push_back(4);
 
-	vertices.push_back(Vertex2D(glm::vec2(0.0f, 0.75f), glm::vec3(0.0f, 0.0f, 0.0f)));
-	vertices.push_back(Vertex2D(glm::vec2(1.0f, 0.75f), glm::vec3(0.0f, 0.0f, 0.0f)));
+	vertices.push_back(Vertex2D(glm::vec2(0.0f, 1.0f), glm::vec3(0.0f, 0.0f, 0.0f)));
+	vertices.push_back(Vertex2D(glm::vec2(1.0f, 1.0f), glm::vec3(0.0f, 0.0f, 0.0f)));
+	vertices.push_back(Vertex2D(glm::vec2(0.0f, 0.5f), glm::vec3(0.0f, 0.0f, 0.0f)));
+
+	vertices.push_back(Vertex2D(glm::vec2(1.0f, 0.5f), glm::vec3(0.0f, 0.0f, 0.0f)));
+
+	vertices.push_back(Vertex2D(glm::vec2(0.75f, 0.0f), glm::vec3(0.0f, 0.0f, 0.0f)));
+
 	vertices.push_back(Vertex2D(glm::vec2(0.0f, 0.0f), glm::vec3(0.0f, 0.0f, 0.0f)));
 
-	vertices.push_back(Vertex2D(glm::vec2(1.0f, 0.0f), glm::vec3(0.0f, 0.0f, 0.0f)));
-
-	vertices.push_back(Vertex2D(glm::vec2(0.75f, -0.75f), glm::vec3(0.0f, 0.0f, 0.0f)));
-
-	vertices.push_back(Vertex2D(glm::vec2(0.0f, -0.75f), glm::vec3(0.0f, 0.0f, 0.0f)));
-
 }
 
-void Char::Init_a()
+void Char::Init_6()
 {
+	indices.push_back(0);
+	indices.push_back(1);
+	indices.push_back(2);
 
+	indices.push_back(2);
+	indices.push_back(3);
+	indices.push_back(4);
+
+	indices.push_back(5);
+	indices.push_back(6);
+	indices.push_back(7);
+
+	vertices.push_back(Vertex2D(glm::vec2(0.0f, 0.5f), glm::vec3(0.0f, 0.0f, 0.0f)));
+	vertices.push_back(Vertex2D(glm::vec2(0.2f, 1.0f), glm::vec3(0.0f, 0.0f, 0.0f)));
+	vertices.push_back(Vertex2D(glm::vec2(0.1f, 0.1f), glm::vec3(0.0f, 0.0f, 0.0f)));
+
+	vertices.push_back(Vertex2D(glm::vec2(0.5f, 0.0f), glm::vec3(0.0f, 0.0f, 0.0f)));
+	vertices.push_back(Vertex2D(glm::vec2(0.5f, 0.3f), glm::vec3(0.0f, 0.0f, 0.0f)));
+
+	vertices.push_back(Vertex2D(glm::vec2(0.5f, 0.2f), glm::vec3(0.0f, 0.0f, 0.0f)));
+	vertices.push_back(Vertex2D(glm::vec2(0.5f, 0.5f), glm::vec3(0.0f, 0.0f, 0.0f)));
+	vertices.push_back(Vertex2D(glm::vec2(0.1f, 0.5f), glm::vec3(0.0f, 0.0f, 0.0f)));
 }
 
-void Char::Init_b()
+void Char::Init_7()
 {
+	indices.push_back(0);
+	indices.push_back(1);
+	indices.push_back(2);
+
+	indices.push_back(2);
+	indices.push_back(3);
+	indices.push_back(4);
+
+	vertices.push_back(Vertex2D(glm::vec2(0.0f, 1.0f), glm::vec3(0.0f, 0.0f, 0.0f)));
+	vertices.push_back(Vertex2D(glm::vec2(0.5f, 0.7f), glm::vec3(0.0f, 0.0f, 0.0f)));
+	vertices.push_back(Vertex2D(glm::vec2(1.0f, 1.0f), glm::vec3(0.0f, 0.0f, 0.0f)));
+
+	vertices.push_back(Vertex2D(glm::vec2(0.4f, 0.6f), glm::vec3(0.0f, 0.0f, 0.0f)));
+	vertices.push_back(Vertex2D(glm::vec2(0.0f, 0.0f), glm::vec3(0.0f, 0.0f, 0.0f)));
 }
 
-void Char::Init_c()
+void Char::Init_8()
 {
+	indices.push_back(0);
+	indices.push_back(1);
+	indices.push_back(2);
+
+	indices.push_back(0);
+	indices.push_back(3);
+	indices.push_back(4);
+
+	indices.push_back(5);
+	indices.push_back(1);
+	indices.push_back(2);
+
+	indices.push_back(5);
+	indices.push_back(3);
+	indices.push_back(4);
+
+	vertices.push_back(Vertex2D(glm::vec2(0.50f, 1.0f), glm::vec3(0.0f, 0.0f, 0.0f)));
+	vertices.push_back(Vertex2D(glm::vec2(0.75f, 0.75f), glm::vec3(0.0f, 0.0f, 0.0f)));
+	vertices.push_back(Vertex2D(glm::vec2(1.0f, 0.75f), glm::vec3(0.0f, 0.0f, 0.0f)));
+	vertices.push_back(Vertex2D(glm::vec2(0.0f, 0.75f), glm::vec3(0.0f, 0.0f, 0.0f)));
+	vertices.push_back(Vertex2D(glm::vec2(0.25f, 0.75f), glm::vec3(0.0f, 0.0f, 0.0f)));
+	vertices.push_back(Vertex2D(glm::vec2(0.50f, 0.5f), glm::vec3(0.0f, 0.0f, 0.0f)));
+
+	indices.push_back(5);
+	indices.push_back(6);
+	indices.push_back(7);
+
+	indices.push_back(5);
+	indices.push_back(8);
+	indices.push_back(9);
+
+	indices.push_back(10);
+	indices.push_back(6);
+	indices.push_back(7);
+
+	indices.push_back(10);
+	indices.push_back(8);
+	indices.push_back(9);
+
+	vertices.push_back(Vertex2D(glm::vec2(0.75f, 0.25f), glm::vec3(0.0f, 0.0f, 0.0f)));
+	vertices.push_back(Vertex2D(glm::vec2(1.0f, 0.25f), glm::vec3(0.0f, 0.0f, 0.0f)));
+	vertices.push_back(Vertex2D(glm::vec2(0.0f, 0.25f), glm::vec3(0.0f, 0.0f, 0.0f)));
+	vertices.push_back(Vertex2D(glm::vec2(0.25f, 0.25f), glm::vec3(0.0f, 0.0f, 0.0f)));
+	vertices.push_back(Vertex2D(glm::vec2(0.50f, 0.0f), glm::vec3(0.0f, 0.0f, 0.0f)));
 }
 
-void Char::Init_d()
+void Char::Init_9()
 {
+	indices.push_back(0);
+	indices.push_back(1);
+	indices.push_back(2);
+
+	indices.push_back(2);
+	indices.push_back(3);
+	indices.push_back(4);
+
+	indices.push_back(5);
+	indices.push_back(6);
+	indices.push_back(7);
+
+	vertices.push_back(Vertex2D(glm::vec2(1.0f, 0.5f), glm::vec3(0.0f, 0.0f, 0.0f)));
+	vertices.push_back(Vertex2D(glm::vec2(0.8f, 0.0f), glm::vec3(0.0f, 0.0f, 0.0f)));
+	vertices.push_back(Vertex2D(glm::vec2(0.9f, 0.9f), glm::vec3(0.0f, 0.0f, 0.0f)));
+
+	vertices.push_back(Vertex2D(glm::vec2(0.5f, 1.0f), glm::vec3(0.0f, 0.0f, 0.0f)));
+	vertices.push_back(Vertex2D(glm::vec2(0.5f, 0.7f), glm::vec3(0.0f, 0.0f, 0.0f)));
+
+	vertices.push_back(Vertex2D(glm::vec2(0.5f, 0.8f), glm::vec3(0.0f, 0.0f, 0.0f)));
+	vertices.push_back(Vertex2D(glm::vec2(0.5f, 0.5f), glm::vec3(0.0f, 0.0f, 0.0f)));
+	vertices.push_back(Vertex2D(glm::vec2(0.9f, 0.5f), glm::vec3(0.0f, 0.0f, 0.0f)));
 }
 
 void Char::Init_r()
@@ -145,17 +371,18 @@ void Char::Init_r()
 	indices.push_back(1);
 	indices.push_back(2);
 
+
 	indices.push_back(3);
 	indices.push_back(4);
 	indices.push_back(5);
 
-	vertices.push_back(Vertex2D(glm::vec2(0.0f, 0.0f), glm::vec3(0.0f, 0.0f, 0.0f)));
-	vertices.push_back(Vertex2D(glm::vec2(0.25f, 0.75f), glm::vec3(0.0f, 0.0f, 0.0f)));
-	vertices.push_back(Vertex2D(glm::vec2(0.25f, -0.75f), glm::vec3(0.0f, 0.0f, 0.0f)));
-
-	vertices.push_back(Vertex2D(glm::vec2(0.25f, 0.5f), glm::vec3(0.0f, 0.0f, 0.0f)));
+	vertices.push_back(Vertex2D(glm::vec2(0.0f, 0.5f), glm::vec3(0.0f, 0.0f, 0.0f)));
+	vertices.push_back(Vertex2D(glm::vec2(0.25f, 1.0f), glm::vec3(0.0f, 0.0f, 0.0f)));
 	vertices.push_back(Vertex2D(glm::vec2(0.25f, 0.0f), glm::vec3(0.0f, 0.0f, 0.0f)));
-	vertices.push_back(Vertex2D(glm::vec2(1.0f, 0.25f), glm::vec3(0.0f, 0.0f, 0.0f)));
+
+	vertices.push_back(Vertex2D(glm::vec2(0.25f, 0.72f), glm::vec3(0.0f, 0.0f, 0.0f)));
+	vertices.push_back(Vertex2D(glm::vec2(0.25f, 0.55f), glm::vec3(0.0f, 0.0f, 0.0f)));
+	vertices.push_back(Vertex2D(glm::vec2(1.0f, 0.63f), glm::vec3(0.0f, 0.0f, 0.0f)));
 }
 
 void Char::Init_t()
@@ -176,15 +403,15 @@ void Char::Init_t()
 	indices.push_back(5);
 	indices.push_back(6);
 
-	vertices.push_back(Vertex2D(glm::vec2(0.0f, 0.0f), glm::vec3(0.0f, 0.0f, 0.0f)));
-	vertices.push_back(Vertex2D(glm::vec2(0.50f, 0.25f), glm::vec3(0.0f, 0.0f, 0.0f)));
-	vertices.push_back(Vertex2D(glm::vec2(0.50f, -0.25f), glm::vec3(0.0f, 0.0f, 0.0f)));
+	vertices.push_back(Vertex2D(glm::vec2(0.0f, 0.50f), glm::vec3(0.0f, 0.0f, 0.0f)));
+	vertices.push_back(Vertex2D(glm::vec2(0.50f, 0.66f), glm::vec3(0.0f, 0.0f, 0.0f)));
+	vertices.push_back(Vertex2D(glm::vec2(0.50f, 0.33f), glm::vec3(0.0f, 0.0f, 0.0f)));
 
-	vertices.push_back(Vertex2D(glm::vec2(1.0f, 0.0f), glm::vec3(0.0f, 0.0f, 0.0f)));
+	vertices.push_back(Vertex2D(glm::vec2(1.0f, 0.50f), glm::vec3(0.0f, 0.0f, 0.0f)));
 
-	vertices.push_back(Vertex2D(glm::vec2(0.50f, 0.75f), glm::vec3(0.0f, 0.0f, 0.0f)));
-	vertices.push_back(Vertex2D(glm::vec2(0.25f, 0.0f), glm::vec3(0.0f, 0.0f, 0.0f)));
-	vertices.push_back(Vertex2D(glm::vec2(0.75f, 0.0f), glm::vec3(0.0f, 0.0f, 0.0f)));
+	vertices.push_back(Vertex2D(glm::vec2(0.50f, 1.0f), glm::vec3(0.0f, 0.0f, 0.0f)));
+	vertices.push_back(Vertex2D(glm::vec2(0.25f, 0.50f), glm::vec3(0.0f, 0.0f, 0.0f)));
+	vertices.push_back(Vertex2D(glm::vec2(0.75f, 0.50f), glm::vec3(0.0f, 0.0f, 0.0f)));
 
-	vertices.push_back(Vertex2D(glm::vec2(0.50f, -0.75f), glm::vec3(0.0f, 0.0f, 0.0f)));
+	vertices.push_back(Vertex2D(glm::vec2(0.50f, 0.0f), glm::vec3(0.0f, 0.0f, 0.0f)));
 }
--- Char.h
+++ Char.h
@@ -6,13 +6,17 @@ class Char :
 public:
     Char(char character, glm::vec2 origin, float size);
 private:
+    void Init_dot();
     void Init_0();
+    void Init_1();
+    void Init_2();
+    void Init_3();
     void Init_4();
     void Init_5();
-    void Init_a();
-    void Init_b();
-    void Init_c();
-    void Init_d();
+    void Init_6();
+    void Init_7();
+    void Init_8();
+    void Init_9();
 
     void Init_r();
     void Init_t();
